1e32dc5fdf28e67b8a061c47150978cbf830c924
MattiasAye <Mattiasayenew@gmail.com>
2025-07-09 21:32:19 -0700

    Mixin Supported
    Took 36 minutes

--- src/main/java/net/newt/prehistoricascension/block/entity/ModHangingSignBlockEntity.java
+++ src/main/java/net/newt/prehistoricascension/block/entity/ModHangingSignBlockEntity.java
@@ -5,7 +5,7 @@ import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.entity.SignBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 
-public class ModHangingSignBlockEntity extends SignBlockEntity {
+public class ModHangingSignBlockEntity extends SignBlockEntity { // Change To HangingSignBlockEntity To Fix Text
     public ModHangingSignBlockEntity(BlockPos pPos, BlockState pBlockState) {
         super(ModBlockEntities.MOD_HANGING_SIGN.get(), pPos, pBlockState);
     }
